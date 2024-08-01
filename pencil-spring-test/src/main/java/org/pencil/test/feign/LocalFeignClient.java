f268c4d00ced945b4568aba9e4d484fd0dcc32be
pencil <2623226464@qq.com>
2024-08-01 13:06:44 +0800

    feat test地址更换

--- pencil-spring-test/src/main/java/org/pencil/test/feign/LocalFeignClient.java
+++ pencil-spring-test/src/main/java/org/pencil/test/feign/LocalFeignClient.java
@@ -1,6 +1,6 @@
 package org.pencil.test.feign;
 
-import org.pencil.beans.resp.Result;
+import org.pencil.entity.resp.Result;
 import org.pencil.feign.config.ComFeignConfig;
 import org.pencil.test.entity.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
